3e238b8979f646a33711b14e3aa36396dee2317b
hsk <sakurai777@gmail.com>
2017-11-20 09:45:57 +0900

    fix findall bug

--- lib/initial.pl
+++ lib/initial.pl
@@ -13,7 +13,18 @@ maplist(A,[X|XS],[R|RS]) :- call(A,X,R),maplist(A,XS,RS).
 maplist(A,[],[],[]).
 maplist(A,[X|XS],[R|RS],[S|SS]) :- call(A,X,R,S),maplist(A,XS,RS,SS).
 
+reverse(A,R) :- reverse1(A,[],R).
+reverse1([],R,R).
+reverse1([A|AS],B,R) :- reverse1(AS,[A|B],R).
+
 foldl(A,R,[],R).
 foldl(A,R,[X|XS],R2) :- call(A,X,R,R1),foldl(A,R1,XS,R2).
 
 forall(A,B) :- call(A),call(B),fail;true.
+
+findall(A,B,R) :- findall1(A,B,R1),reverse(R1,R).
+findall1(A,B,R) :- asserta(findall1([])),
+  forall(B,findall2(A)),
+  findall1(R),
+  retract(findall1(Z)).
+findall2(A):-findall1(L),retract(findall1(L)),asserta(findall1([A|L])).
--- test/findall.pl
+++ test/findall.pl
@@ -1,15 +1,7 @@
 name(test).
 name(hoge).
 name(fuga).
-forall1(A,B) :- call(A),call(B),fail;true.
 
-findall2(A):-findall1(L),retract(findall1(L)),asserta(findall1([A|L])).
-
-findall1(A,B,R) :- asserta(findall1([])),
-  forall1(B,findall2(A)),
-  findall1(R),
-  retract(findall1(Z)).
-
-:- findall1(A,name(A),R),writeln(R).
-:- forall1(findall1(A),writeln(A)).
+:- findall(A,name(A),R),writeln(R).
+:- forall(findall1(A),writeln(A)).
 :- halt.
