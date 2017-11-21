6117a639b21bea069ff4b3d13d8b3943a7094ed2
hsk <sakurai777@gmail.com>
2017-11-21 22:08:31 +0900

    change test files

--- examples/eval3.pl
+++ examples/eval3.pl
@@ -1,5 +1,7 @@
-% Syntax
+:- op(1200,xfx,--).
+term_expansion(A--B,B:-A).
 
+% Syntax
 i(I) :- integer(I).
 e(A) :- i(A).
 e(E1+E2) :- e(E1),e(E2).
@@ -8,30 +10,30 @@ e(E1*E2) :- e(E1),e(E2).
 % Typing
 
 integer(I)
--------------------------------- (T-Int)
-typing(I,int)
+--%------------------------------ (T-Int)
+typing(I,int).
 
-typing(E1,int)    typing(E2,int)
--------------------------------- (T-Add)
-typing(E1+E2,int)
+typing(E1,int),    typing(E2,int)
+--%------------------------------ (T-Add)
+typing(E1+E2,int).
 
-typing(E1,int)    typing(E2,int)
--------------------------------- (T-Mul)
-typing(E1*E2,int)
+typing(E1,int),    typing(E2,int)
+--%------------------------------ (T-Mul)
+typing(E1*E2,int).
 
 % Evaluation
 
 integer(A)
----------- (E-Int)
-eval(A,A)
+--%-------- (E-Int)
+eval(A,A).
 
-eval(A,R1) eval(B,R2) R is R1 + R2
----------------------------------- (E-Add)
-eval(A+B,R)
+eval(A,R1),   eval(B,R2),   R is R1 + R2
+--%-------------------------------------- (E-Add)
+eval(A+B,R).
 
-eval(A,R1) eval(B,R2) R is R1 * R2
----------------------------------- (E-Add)
-eval(A*B,R)
+eval(A,R1),   eval(B,R2),   R is R1 * R2
+--%-------------------------------------- (E-Add)
+eval(A*B,R).
 
 run(E,R) :- e(E), typing(E,int), eval(E,R).
 
--- test/comment2.pl
+++ test/comment2.pl
@@ -1,9 +1,10 @@
 % test
 
+:- op(1200,xfx,--).
+term_expansion(A--B,B:-A).
+
 integer(I)
---------------
-i(I) :- integer(I)
-
-
+--%---------------
+i(I).
 
 :- i(1),halt.
